536b9e2e0ea3fd0f9c4f74ba063e2d98c833b78b
julianzulatto <julianzulatto14@outlook.com>
2025-01-29 18:18:09 -0300

    bootstrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import image from "../resources/20231021_162320.jpg"
 
 function Home() {
-    return <h2>Home Page</h2>;
+    return (
+        <section id="about" className="container text-center mt-5">
+            <h1>Hola, soy [Julian Zulatto]</h1>
+            <p>Desarrollador Front-end React</p>
+            <img src={image} alt="Tu foto" style={{ width: "200px", height: "300px" }} />
+        </section>)
 }
 
 export default Home;
