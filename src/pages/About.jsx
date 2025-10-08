cfee96dc5ca337a0c6e71ffe3ad38ad03c207896
julianzulatto <julianzulatto14@outlook.com>
2025-10-08 17:38:37 -0300

    edit-about-me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 function About() {
     return (
         <section id="about">
-            <h2 className='fw-bold' style={{color:"#22D3EE"}}>Sobre mi</h2>
-            <p style={{color:"#22D3EE"}}>Soy un analista contable con 7 años de experiencia en la gestión administrativa de un corralón de materiales de construcción. Durante este tiempo, he desempeñado una variedad de funciones que van desde la atención al cliente hasta la administración de sistemas informáticos y la coordinación de proveedores.
+            <h2 className='fw-bold' style={{ color: "#22D3EE" }}>Sobre mi</h2>
+            <p style={{ color: "#22D3EE" }}>Soy un Desarrollador Full-Stack en formación con una base sólida en Javascript, React y Node.js. Mi trayectoria es inusual: durante 7 años, gestioné las operaciones y finanzas en el sector de la construcción, donde desarrollé una fuerte orientación al negocio, pensamiento analítico y habilidades en gestión de sistemas y procesos.
 
-                En 2023, decidí explorar nuevas oportunidades y me adentré en el mundo de la programación con Javascript. Desde entonces, me he comprometido a convertirme en un desarrollador experto, dedicándome a una formación continua y a la actualización constante en las últimas tecnologías.
+                En 2023, hice una transición intencional hacia la programación. Este cambio no solo refleja mi disciplina y capacidad de autoaprendizaje, sino mi pasión por resolver problemas de manera eficiente a través del código. Actualmente, estoy comprometido con el aprendizaje continuo de las últimas tecnologías y busco activamente una oportunidad en un equipo innovador donde pueda aplicar mis conocimientos técnicos y mi visión de negocio para contribuir al desarrollo de soluciones robustas.
 
-                Estoy entusiasmado por unirme a un equipo dinámico donde pueda aplicar las habilidades que estoy adquiriendo y enriquecerme con la práctica y la experiencia compartida.</p>
-
-            <h3 style={{color:"#22D3EE"}}>Estoy practicando React y mejorando mis habilidades como desarrollador frontend. Estoy buscando mi primera experiencia laboral para seguir creciendo y sumar experiencia real en proyectos.</h3>
+                Estoy listo para aportar mi ética de trabajo y entusiasmo en un entorno colaborativo.</p>
+            <p style={{ color: "#22D3EE" }}>Estoy practicando React y mejorando mis habilidades como desarrollador frontend. Estoy buscando mi primera experiencia laboral para seguir creciendo y sumar experiencia real en proyectos.</p>
         </section>
     )
 }
