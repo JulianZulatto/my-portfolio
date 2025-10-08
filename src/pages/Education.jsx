8a552ae5fb2f5e2ebf460718399de7635fbc02ce
julianzulatto <julianzulatto14@outlook.com>
2025-10-08 18:48:41 -0300

    deploy-forced

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -11,64 +11,62 @@ import ModalComponent from '../components/Modals/ModalComponent';
 
 function Education() {
 
-    const[modalVisible, setModalVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
-    const[currentImageSrc, setCurrentImageSrc] = useState("");
+    const [currentImageSrc, setCurrentImageSrc] = useState("");
 
-    const handleModalOpen =(src)=>{
+    const handleModalOpen = (src) => {
         setCurrentImageSrc(src);
         setModalVisible(true);
     };
 
-    const handleModalClose = () =>{
+    const handleModalClose = () => {
         setModalVisible(false);
         setCurrentImageSrc("");
     }
 
-
-
     return (
         <section id="education" className="container mt-5">
-            <h2 className="text-center" style={{color:"#22D3EE"}}>Educacion</h2>
+            <h2 className="text-center" style={{ color: "#22D3EE" }}>Educacion</h2>
             <Row>
                 <Col>
-                    <CardComponent 
-                    img={desarrollowebCert} 
-                    title={"Desarrollo Web - Coderhouse"} 
-                    text={"Certificado de finalización obtenido el 13 de marzo de 2024. Curso enfocado en HTML, CSS, Git y principios fundamentales del desarrollo frontend."}
-                    onButtonClick={()=>{handleModalOpen(desarrollowebCert)}}
-                    name={"Ver Certificado"}             
-                    />  
-                </Col>
-
-                <Col>
-                <CardComponent 
-                    img={javascriptCert} 
-                    title={"JavaScript - Coderhouse"} 
-                    text={"Certificado de finalización obtenido el 8 de julio de 2024. Curso centrado en lógica de programación, funciones, DOM, eventos y asincronía con JavaScript puro."}
-                    onButtonClick={()=>{handleModalOpen(javascriptCert)}}   
-                    name={"Ver Certificado"}           
+                    <CardComponent
+                        img={desarrollowebCert}
+                        title={"Desarrollo Web - Coderhouse"}
+                        text={"Certificado de finalización obtenido el 13 de marzo de 2024. Curso enfocado en HTML, CSS, Git y principios fundamentales del desarrollo frontend."}
+                        onButtonClick={() => { handleModalOpen(desarrollowebCert) }}
+                        name={"Ver Certificado"}
                     />
                 </Col>
 
                 <Col>
-                    <CardComponent 
-                    img={reactCert} 
-                    title={"React - Coderhouse"} 
-                    text={"Certificado de finalización obtenido el 19 de septiembre de 2024.  Curso enfocado en el desarrollo de aplicaciones con React, JSX, estados, props, hooks y enrutamiento."}
-                    onButtonClick={()=>{handleModalOpen(reactCert)}}  
-                    name={"Ver Certificado"}            
+                    <CardComponent
+                        img={javascriptCert}
+                        title={"JavaScript - Coderhouse"}
+                        text={"Certificado de finalización obtenido el 8 de julio de 2024. Curso centrado en lógica de programación, funciones, DOM, eventos y asincronía con JavaScript puro."}
+                        onButtonClick={() => { handleModalOpen(javascriptCert) }}
+                        name={"Ver Certificado"}
+                    />
+                </Col>
+
+                <Col>
+                    <CardComponent
+                        img={reactCert}
+                        title={"React - Coderhouse"}
+                        text={"Certificado de finalización obtenido el 19 de septiembre de 2024.  Curso enfocado en el desarrollo de aplicaciones con React, JSX, estados, props, hooks y enrutamiento."}
+                        onButtonClick={() => { handleModalOpen(reactCert) }}
+                        name={"Ver Certificado"}
                     />
                 </Col>
             </Row>
             <ModalComponent
-            show={modalVisible}
-            onClose={handleModalClose}
-            src={currentImageSrc}
+                show={modalVisible}
+                onClose={handleModalClose}
+                src={currentImageSrc}
             />
         </section>
     )
-    
+
 };
 
 export default Education;
